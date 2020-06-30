74a2b601a0a81181da0b3dbb1ef20c6f5a39a9eb
John Serrao <bigbadserrao@gmail.com>
2020-06-30 14:28:24 -0400

    Moved variables into their own file

---
+++ components/global/variables.js
@@ -0,0 +1,33 @@
+// Variables for site
+const Variables = {
+  // Colors
+  black: 'rgb(26,26,26)',
+  darkGray: '#4D4D4F',
+  mediumGray: 'rgb(216,216,216)',
+  lightGray: '#8A8A8D',
+  red: '#D23823',
+  blue: '#3E9FDC',
+
+  // Breakpoints
+  ty: '380px',
+  xs: '480px',
+  sm: '768px',
+  md: '980px',
+  lg: '1200px',
+  xl: '1600px',
+
+  // Z-index
+  zIndexNegative: '-10',
+  zIndex00: '0',
+  zIndex01: '10',
+  zIndex02: '20',
+  zIndex03: '30',
+  zIndex04: '40',
+  zIndex05: '50',
+  zIndex06: '60',
+  zIndex07: '70',
+  zIndex08: '80',
+  zIndex09: '90'
+}
+
+export default Variables
