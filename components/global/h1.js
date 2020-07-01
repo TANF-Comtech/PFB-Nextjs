bcfef808a2a949be69bab689be3b3833a9c35cb7
John Serrao <bigbadserrao@gmail.com>
2020-07-01 17:14:18 -0400

    More documentation

--- components/global/h1.js
+++ components/global/h1.js
@@ -4,11 +4,23 @@ import styled from "styled-components"
 const Heading = styled.h1`
   margin: 4vh 0;
   padding: 0;
+  text-transform: uppercase;
 
   @media (min-width: ${props => props.theme.lg}) {
     padding: 0;
   }
 `
+
+/**
+ * <Heading1>
+ * 
+ * About as simple as a component could be
+ * Heading1 is the just the basic heading on the top of most pages
+ * It's inheriting most of it's style from /components/styles/global-css.js
+ * 
+ * @param {object} children - inherited nested components, core React idea
+ * 
+ */
 const Heading1 = ({ children }) => {
   return (
     <>
