0ba30074c8e3d745ddeb336faf60f94ab855738f
John Serrao <bigbadserrao@gmail.com>
2020-07-02 09:05:08 -0400

    added default box shadow

--- components/styles/variables.js
+++ components/styles/variables.js
@@ -35,8 +35,9 @@ const Variables = {
   zIndex08: '80',
   zIndex09: '90',
 
-  // Animation
-  basicAnimation: '0.2s ease-in-out'
+  // Animation / Effects
+  basicAnimation: '0.2s ease-in-out',
+  basicBoxShadow: '0 13px 27px -5px rgba(50,50,93,.25), 0 8px 16px -8px rgba(0,0,0,.3), 0 -6px 16px -6px rgba(0,0,0,.025);'
 }
 
 export default Variables
