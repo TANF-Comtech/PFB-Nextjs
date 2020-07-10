48de6af79f06ec4f91c6788dff3a7d1fcd862608
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2020-07-09 23:53:35 -0400

    :construction: :lipstick: Storybook is giving me trouble rendering the image i'm trying to use for the arrow, whether it's svg or png. This might be a Next.js thing, not sure, i'll have to talk to you about it.

--- components/global/accordion/accordion.js
+++ components/global/accordion/accordion.js
@@ -1,7 +1,10 @@
 import React from "react";
-import Wrapper from "../wrapper";
-import RightArrow from "../../../public/accordionArrow.svg";
+import Text from "../text";
+import RightArrow from "./expansionArrow.png";
 
+/****
+ * Arrow button used to control the expansion panel
+ ****/
 const ArrowButton = ({ onClick, accordionIsOpen }) => (
   <>
     <div
@@ -13,12 +16,6 @@ const ArrowButton = ({ onClick, accordionIsOpen }) => (
   </>
 );
 
-const AccordionContainer = ({ accordionisOpen }) => (
-  <div className={`containerHide ${accordionisOpen ? "containerReveal" : ""}`}>
-    <Wrapper />
-  </div>
-);
-
 export default function Accordion() {
   /****
    * The hook that's going to be used to handle the state change
@@ -27,15 +24,30 @@ export default function Accordion() {
   /****
    * Toggle the button
    ****/
-  const accordionToggleDrawer = () => setAccordionIsOpen(!accordionisOpen);
+  const accordionToggleDrawer = () => setAccordionIsOpen(!accordionIsOpen);
 
   return (
     <>
-      <ArrowButton
-        onClick={accordionToggleDrawer}
-        accordionIsOpen={accordionIsOpen}
-      />
-      <AccordionContainer accordionisOpen={accordionIsOpen} />
+      {/* The main wrapper will be used statically. It'll just hold everything within it. */}
+      <div>
+        <ArrowButton
+          onClick={accordionToggleDrawer}
+          accordionIsOpen={accordionIsOpen}
+        />
+        <Text>What We Fund</Text>
+        {/*Container to hold the panels content when it's closed, and then display content when it's open, currently holds dummy data. Later on we can add a prop to this so that it can be a reusable component. Fow now we're going for functionality.*/}
+        <Text className={`textHide ${accordionIsOpen ? "textReveal" : ""}`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis placerat
+          ante in metus pharetra ultrices. Orci varius natoque penatibus et
+          magnis dis parturient montes, nascetur ridiculus mus. Aenean congue,
+          nunc ac iaculis dignissim, ante dui viverra arcu, eu convallis lacus
+          velit sed ex. Curabitur vulputate turpis gravida eleifend varius.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Nunc bibendum sem ac velit varius, quis
+          consequat risus varius. Nulla semper nunc ac nisl maximus, mollis
+          sollicitudin nibh pretium. Nullam quis orci metus.
+        </Text>
+      </div>
     </>
   );
 }
