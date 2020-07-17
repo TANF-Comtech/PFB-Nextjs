df0ef91ca4dccb549ed04cc27b06c4a635fb998b
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2020-07-16 23:47:57 -0400

    I got the accordion working with styled components. The one piece I didn't get is the arrow rotation. I'm calling it an overall win though.

--- components/global/accordion.js
+++ components/global/accordion.js
@@ -13,66 +13,57 @@
       </Accordion>
  ****/
 
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Arrow from "./Arrow";
-import useOnClickOutside from "../hooks/onClickUseOutside";
+
+const AccordionWrapper = styled.div`
+  display: flex;
+  background-color: white;
+  border-top: 1px solid black;
+  height: auto;
+  padding: 2%;
+  transition: all 0.3s ease-in-out;
+`;
+
+const InternalWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  max-height: ${(props) => (props.open ? "100px" : "0")};
+  transition: all 0.3s ease-in-out;
+  overflow: hidden;
+`;
+
+const ArrowButton = styled.div`
+  border: none;
+  margin: none;
+  display: flex;
+  background-color: white;
+  height: min-content;
+  transition: all 0.3s ease-in-out;
+  transform: ${(props) => (props.setOpen ? `rotate(90deg)` : "")};
+`;
+
+const Title = styled.div`
+  margin: none;
+  font-size: 24px;
+`;
 
 const Accordion = ({ title, children }) => {
-  const [active, setActive] = useState("");
-  const [height, setHeight] = useState("0px");
-  const [rotation, setRotation] = useState("accordion__icon");
-
-  const content = useRef();
-  const sensitive = useRef();
-
-  const AccordionContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    border-top: 1px solid black;
-  `;
-
-  const AccordionButton = styled.button`
-    background-color: white;
-    border: none;
-  `;
-
-  const toggleAccordion = () => {
-    setActive(active === "" ? "active" : "");
-    setHeight(
-      active === "active" ? "0px" : `${content.current.scrollHeight}px`
-    );
-    setRotation(
-      active === "active" ? "accordion__icon" : "accordion__icon rotate"
-    );
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
   };
-
-  useOnClickOutside(sensitive, () => {
-    setActive("");
-    setHeight("0px");
-    setRotation("accordion__icon");
-  });
-
   return (
-    <AccordionContainer>
-      <div className="accordion__section" ref={sensitive}>
-        <AccordionButton
-          className={`accordion ${active}`}
-          onClick={toggleAccordion}
-        >
-          <Arrow width={24} className={`${rotation}`} />
-        </AccordionButton>
-        <p className="accordion__title">{title}</p>
-
-        <div
-          ref={content}
-          style={{ maxHeight: `${height}` }}
-          className="accordion__content"
-        >
-          <div className="accordion__text">{children}</div>
-        </div>
-      </div>
-    </AccordionContainer>
+    <>
+      <AccordionWrapper>
+        <ArrowButton padding="5px" onClick={handleClick} open={open}>
+          <Arrow width={24} open={open} />
+        </ArrowButton>
+        <Title>{title}</Title>
+      </AccordionWrapper>
+      <InternalWrapper open={open}>{children}</InternalWrapper>
+    </>
   );
 };
 
--- stories/testAccordion.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import TestAccordion from "../components/global/testAccordion";
-
-export default {
-  title: "Test Accordion",
-  component: TestAccordion,
-};
-
-export const Basic = () => <TestAccordion />;
