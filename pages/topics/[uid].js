18b709771dece1ff2eeed7b9e82f97c98173a545
John Serrao <bigbadserrao@gmail.com>
2020-08-10 08:07:32 -0600

    Topic queries and template file set up

---
+++ pages/topics/[uid].js
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import { Title } from 'prismic-dom'
+
+import { getTopics, getSingleTopicPage } from '../../lib/taxonomy/topics'
+
+import Wrapper from '../../components/global/wrapper'
+import Heading1 from '../../components/global/h1'
+import MainContent from '../../components/global/main-content'
+
+
+/* You must reference the `topic` prop to get data from `getStaticProps` - check bottom of this file */
+export default function TopicPage({ page, preview }) {
+  // const router = useRouter()
+  // if (!router.isFallback && !page?.node?._meta?.uid) {
+  //   return <ErrorPage statusCode={404} />
+  // }
+
+  // Destructure topic out of page prop
+  const { topic } = page
+
+  return (
+    <>
+      <Head>
+        <title>
+          {topic.title[0].text} | People for Bikes
+        </title>
+      </Head>
+      <Wrapper postTitle={topic.title[0].text}>
+        <h1>{topic.title[0].text}</h1>
+      </Wrapper>
+    </>
+  )
+}
+
+/* The return here sends the `page` prop back to the TopicPage component above for rendering */
+export async function getStaticProps({ params, preview = false, previewData }) {
+  const pageData = await getSingleTopicPage(params.uid, previewData)
+
+  return {
+    props: {
+      preview,
+      page: pageData ?? null,
+    },
+    revalidate: 1,
+  }
+}
+
+// getStaticPaths requires the whole paths argument to be objects of URL it needs to statically render server-side
+export async function getStaticPaths() {
+  const allTopics = await getTopics()
+  return {
+    paths: allTopics?.map(({ node }) => `/topics/${node._meta.uid}`) || [],
+    fallback: true,
+  }
+}
