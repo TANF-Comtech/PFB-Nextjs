89ba33b1281f259d9d3a25ffbaaee640adca6bd4
John Serrao <bigbadserrao@gmail.com>
2020-09-21 07:53:02 -0600

    Basic topic page roughed in - content is still not there

--- pages/topics/[uid].js
+++ pages/topics/[uid].js
@@ -3,9 +3,11 @@ import ErrorPage from 'next/error'
 
 import { getTopics, getSingleTopicPage } from '../../lib/queries/topics'
 
-import Wrapper from '../../components/global/wrapper'
-import Heading1 from '../../components/primitives/h1'
+import WideWrapper from '../../components/global/wrapper-wide'
+import HeaderImage from '../../components/global/header-image'
+
 import MainContent from '../../components/global/main-content'
+import ContentItem from '../../components/content/content-item'
 
 
 /* You must reference the `topic` prop to get data from `getStaticProps` - check bottom of this file */
@@ -16,7 +18,7 @@ export default function TopicPage({ page, preview }) {
 
   // Destructure topic out of page prop
   const { topic } = page
-
+  console.log(topic)
   return (
     <>
       <Head>
@@ -24,9 +26,44 @@ export default function TopicPage({ page, preview }) {
           {topic.title[0].text} | People for Bikes
         </title>
       </Head>
-      <Wrapper postTitle={topic.title[0].text}>
-        <h1>{topic.title[0].text}</h1>
-      </Wrapper>
+      <WideWrapper postTitle="People for Bikes Homepage">
+        <HeaderImage 
+          headingRGBA="255,255,255,1"
+          source={ topic.banner_image.url }
+        >
+          <h1>{topic.title[0].text}</h1>
+        </HeaderImage>
+        <MainContent>
+          <h2>People for Bikes work in {topic.title[0].text}</h2>
+          <h3>News</h3>
+          <ContentItem 
+            date="September 5th, 2020"
+            title="Opening of New Cycletrack in Sedona"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+          />
+          <ContentItem 
+            date="July 12th, 2020"
+            title="DRAFT Digital Happy Hour Success"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+          />
+          <ContentItem 
+            date="October 23rd, 2019"
+            title="Tucson makes huge leap in City Ratings"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+          />
+          <h3>Grants and Policy</h3>
+          <ContentItem 
+            date="September 7th, 2020"
+            title="2020 ebike laws for Arizona released"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+          />
+          <ContentItem 
+            date="April 12th, 2020"
+            title="Opening of New Cycletrack in Sedona"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+          />   
+        </MainContent>        
+      </WideWrapper>
     </>
   )
 }
