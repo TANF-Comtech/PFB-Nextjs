c19e9df00c7a73dfc683f9eaecb817c037ac7e3e
John Serrao <bigbadserrao@gmail.com>
2020-09-23 17:16:27 -0600

    Wrapper refactored to be wide and narrow

--- components/global/wrapper.js
+++ components/global/wrapper.js
@@ -3,10 +3,11 @@ import styled from "styled-components";
 import useSize from "@react-hook/size";
 
 import Titlebar from "../global/titlebar";
+import { typeOf } from "react-is";
 
 // spacer constrains the layout on big screens but keeps it tight on mobile
 const MainContent = styled.main`
-  padding: 14vh 4vw 4vh 4vw; /* slightly bigger pad on bottom than top */
+  padding: ${ props => props.isWidePass ? `14vh 0 4vh 0` : `14vh 4vw 4vh 4vw` };
   min-height: 80vh;
 `;
 
@@ -23,10 +24,11 @@ const Container = styled.section`
  * In the main section, we have the "flow" class gives us some nice automatic padding around sibling elements
  *
  * @param {object} children - inherited nested components, core React idea
+ * @param {boolean} isWide - how wide of a container we want (true = full width || false = 1200px)
  * @param {text} postTitle - actual title of content of page, feeds into Titlebar component
  *
  */
-const Wrapper = ({ children, postTitle }) => {
+const Wrapper = ({ children, isWide, postTitle }) => {
   /* useSize hook gives us the dimensions of the target element (wherever you apply `ref`) */
   /* See example: https://github.com/jaredLunde/react-hook/tree/master/packages/size */
   const mainTarget = useRef(null);
@@ -35,11 +37,21 @@ const Wrapper = ({ children, postTitle }) => {
     initialHeight: 0,
   });
 
+  console.log(isWide)
+
   return (
     <>
-      <Titlebar mainHeight={Math.round(mainHeight)}>{postTitle}</Titlebar>
-      <MainContent ref={mainTarget}>
-        <Container>{children}</Container>
+      <Titlebar mainHeight={Math.round(mainHeight)}>
+        { postTitle }
+      </Titlebar>
+      <MainContent 
+        ref={ mainTarget }
+        isWidePass={ isWide }
+      >
+        { isWide 
+          ? ( <> { children } </> )
+          : ( <Container> { children } </Container> )
+        }
       </MainContent>
     </>
   );
