032a0f393029b155e270026f2ba809e1915b8c44
John Serrao <bigbadserrao@gmail.com>
2020-09-23 23:21:16 -0600

    Implemented a SITE_URL for isometric apps in next.config.js, compiler works but data not present

--- next.config.js
+++ next.config.js
@@ -1,3 +1,36 @@
 // next.config.js
 const withImages = require('next-images')
-module.exports = withImages()
+
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
+
+// This uses phases as outlined here: 
+// https://github.com/vercel/next.js/tree/canary/examples/with-env-from-next-config-js
+module.exports = withImages((phase) => {
+  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  // when `next build` or `npm run build` is used
+  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+  // when `next build` or `npm run build` is used
+  const isStaging =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+
+  const env = {
+    SITE_URL: (() => {
+      if (isDev) return 'http://localhost:3001'
+      if (isProd) {
+        return 'https://pfb-nextjs.vercel.app/'
+      }
+      if (isStaging) return 'https://pfb-nextjs.vercel.app/'
+      return 'SITE_URL:not (isDev,isProd && !isStaging,isProd && isStaging)'
+    })(),
+  }
+
+  // next.config.js object
+  return {
+    env,
+  }
+})
+
--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,14 @@ import GlobalStyle from '../components/styles/global-css'
 
 import NavBar from '../components/global/navbar'
 import Footer from '../components/global/footer'
+import SiteMeta from '../components/meta/site-meta'
 
-import logo from '../public/logo.svg'
 import splashOne from '../public/social-splash/PFB_Social-01.jpg'
 import splashTwo from '../public/social-splash/PFB_Social-02.jpg'
 import splashThree from '../public/social-splash/PFB_Social-03.jpg'
 import splashFour from '../public/social-splash/PFB_Social-04.jpg'
+
+import logo from '../public/logo.svg'
 
 /**
  * <MyApp>
@@ -30,9 +32,10 @@ import splashFour from '../public/social-splash/PFB_Social-04.jpg'
  * <Component> is a Next.js default that renders all components and provides props to them
  */
 const MyApp = ({ Component, pageProps }) => {
-  // Instantiates Apollo client
-  const apolloClient = useApollo(pageProps.initialApolloState)
-  const socialSplashArr = [ splashOne, splashTwo, splashThree, splashFour ]
+
+  const apolloClient = useApollo(pageProps.initialApolloState) // Instantiates Apollo client
+  const socialSplashArr = [ splashOne, splashTwo, splashThree, splashFour ] // social images
+  // Google LD+JSON basic
   const ldJSONBasic = {
     "@context": "http://schema.org",
     "@type": "Organization",
@@ -52,32 +55,35 @@ const MyApp = ({ Component, pageProps }) => {
     <ApolloProvider client={ apolloClient }>
       <ThemeProvider theme={ Variables }>
         <Head>
-          <title>People for Bikes</title>
           <link rel="icon" href={ logo } sizes="any" type="image/svg+xml" />
           <link rel="stylesheet" href="https://cloud.typography.com/6938898/7916412/css/fonts.css" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" />
-
-          <meta name="description" content="PeopleForBikes is committed to improving biking for everyone. Learn more about our work and join our movement." />
-          <meta property="og:image" content={ socialSplashArr[Math.floor(Math.random()*socialSplashArr.length)] } />
-          <meta property="og:image:width" content="1600" />
-          <meta property="og:image:height" content="900" />
-          <meta property="og:locale" content="en_US" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="PeopleForBikes" />
-          <meta property="og:description" content="PeopleForBikes is committed to improving biking for everyone. Learn more about our work and join our movement." />
-          <meta property="og:url" content="https://www.peopleforbikes.org/" />
-          <meta property="og:site_name" content="PeopleForBikes" />
           <meta property="fb:app_id" content="#" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@peopleforbikes" />
-          <meta name="twitter:title" content="PeopleForBikes | Every ride. Every rider. Join us." />
-          <meta name="twitter:description" content="PeopleForBikes is committed to improving biking for everyone. Learn more about our work and join our movement." />
-          <meta name="twitter:image" content={ socialSplashArr[Math.floor(Math.random()*socialSplashArr.length)] } />
-          <meta name="twitter:image:width" content="1600" />
-          <meta name="twitter:image:height" content="900" />
-          <link rel="canonical" href="https://www.peopleforbikes.org/" />
           <script type="application/ld+json">{JSON.stringify(ldJSONBasic)}</script>
+          <meta property="og:site_name" 
+            content="PeopleForBikes"
+            key="ogsite" />
+          <meta property="og:locale" 
+                content="en_US" 
+                key="oglocale" />
+          <meta property="og:type" 
+                content="website" 
+                key="ogtype" />
+          <meta name="twitter:card" 
+                content="summary_large_image" 
+                key="twtrcard" />
+          <meta name="twitter:site" 
+                content="@peopleforbikes" 
+                key="twtrsite" />
         </Head>
+        <SiteMeta
+          desc="PeopleForBikes is committed to improving biking for everyone. Learn more about our work and join our movement."
+          title="PeopleForBikes | Every ride. Every rider. Join us."
+          imgHeight="900"
+          imgSrc={ socialSplashArr[Math.floor(Math.random()*socialSplashArr.length)] }
+          imgWidth="1600"
+          path="https://www.peopleforbikes.org"
+        />
         <GlobalStyle />
         <NavBar />
         <Component {...pageProps} />
