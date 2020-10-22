39aaf09457200c0f2160c6cb86a258197b165622
John Serrao <bigbadserrao@gmail.com>
2020-10-22 18:11:25 -0500

    ContentItem now accepts nextjs links

--- components/content/content-item.js
+++ components/content/content-item.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Link from "next/link"
 import styled from "styled-components";
 
 const Container = styled.div`
   margin: 4vh 0;
+
+  a, a:visited, a:active, a:focus, a:hover {
+    text-decoration: none;
+  }
 `
 
 const Title = styled.h4`
@@ -30,18 +35,24 @@ const Datestamp = styled.p`
  * A simple presentation for content items that show up in lists
  *
  * @param { string } date - when the content item occurred (optional)
+ * @param { string } path - tells us the URL to the item
  * @param { string } title - title that goes next to the icon
  * @param { string } text - content to be shown
  */
 const ContentItem = ({
   author,
   date,
+  path,
   title,
   text
 }) => {
   return (
     <Container>
-      <Title>{ title }</Title>
+      <Link href={ path } passHref="true">
+        <a>
+          <Title>{ title }</Title>
+        </a>
+      </Link>
       <Datestamp>{ date }</Datestamp>
       <Text>{ text }</Text>
     </Container>
