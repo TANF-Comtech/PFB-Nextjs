55f42df4d065eba4a911294eaa596026662deaf4
John Serrao <bigbadserrao@gmail.com>
2020-11-22 19:15:22 -0600

    Login page baseline

---
+++ pages/log-in.js
@@ -0,0 +1,18 @@
+import Wrapper from '../components/global/wrapper'
+
+function Homepage() {
+  return (
+    <>
+      <Wrapper 
+        postTitle="People for Bikes Homepage"
+        isWide={ false }
+      >
+        <h1>Login</h1>
+        <h2>Access the PeopleForBikes Member Center</h2>
+
+      </Wrapper>
+    </>
+  )
+}
+
+export default Homepage
