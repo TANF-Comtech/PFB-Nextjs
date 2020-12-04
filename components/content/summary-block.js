1c09bcf2d66df076e46abcc9fe2ea79aead5fb19
John Serrao <bigbadserrao@gmail.com>
2020-12-04 10:11:41 -0600

    SummaryBlock added for landing pages

---
+++ components/content/summary-block.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+import MainContent from '../global/main-content'
+
+const Para = styled.p`
+  margin: 20px 0;
+  
+  @media screen and (min-width: 320px) {
+    margin: calc(20px + 26 * ((100vw - 320px) / 880)) 0;
+  }
+  @media screen and (min-width: 1200px) {
+    margin: 46px 0;
+  }  
+`
+
+/**
+ * <SummaryBlock>
+ * 
+ * A really simple paragraph, set off with big margins for landing pages. 
+ * This could probably be used in a bunch of places.
+ *
+ * @param { object } children - React child elements
+ */
+
+const SummaryBlock = ({
+  children
+}) => {
+  return (
+    <MainContent>
+      <Para>{ children }</Para>
+    </MainContent>
+  )
+}
+
+export default SummaryBlock
