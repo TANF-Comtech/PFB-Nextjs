4334f82ec5bb39cd0c32f6d73b2f4cc766131e60
ospira <24227725+ospira@users.noreply.github.com>
2020-12-04 15:31:32 -0500

    (WIP) salesforce tokens

--- pages/api/auth/member_center/login.js
+++ pages/api/auth/member_center/login.js
@@ -1,38 +1,12 @@
-const checkEmailInSalesForce = async (email) => {
-    setLoginLoading(true)
-    const response = await fetch (`/api/auth/member_center/login`,{
-      method:'POST',
-      mode:'same-origin',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({email})
-
-    })
-    return response.json()
-  }
-
-  const sendAuth0Code = async (email) => {
-    setLoginLoading(true)
-    const response = await fetch (`/api/auth/member_center/login`,{
-      method:'POST',
-      mode:'same-origin',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({email})
-
-    })
-    return response.json()
-  }
-
+import checkEmailInSalesforce from '../../../../lib/salesforce/checkEmailInSalesforce'
+import sendPasswordlessAuthCode from '../../../../lib/auth0/sendPasswordlessAuthCode'
 
 export default (req, res) => {
     if (req.method === 'POST') {
         const email = req.body.email
-        checkEmailInSalesForce(email).then(data=>{
+        checkEmailInSalesforce(email).then(data=>{
             if(data.inSalesForce){
-                sendAuth0Code(email).then(data=>{
+                sendPasswordlessAuthCode(email).then(data=>{
                     res.status(200).json({status:true}) //will tweak
                 })
             }
