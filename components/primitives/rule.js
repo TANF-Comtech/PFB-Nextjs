5d86f626da3a52d9ee0e873bfbc7e6b394ddae4e
John Serrao <bigbadserrao@gmail.com>
2020-12-12 14:52:35 -0600

    Rule primitive, works between sections well

---
+++ components/primitives/rule.js
@@ -0,0 +1,44 @@
+import React from "react"
+import styled from "styled-components"
+
+const MainContainer = styled.section`
+  padding: 5px 0;
+`;
+
+const Sizer = styled.div`
+  background-color: ${ props => props.bgColor || props.theme.lightestGray };
+  height: 1px;
+  margin: 0 auto;
+  max-width: ${props => props.maxWidth || props.theme.lg};
+`;
+
+
+/**
+ * <Rule>
+ * 
+ * Horizontal rule that you can adjust the width of.
+ * Very useful, very simple.
+ *
+ * @param { string } bgColor - color for background (default: none)
+ * @param { string } className - should allow styled-components to pass down it's styles
+ * @param { string } maxWidth - however wide you want the rule (default is 1200px)
+ * 
+ */
+const Rule = ({ 
+  bgColor,
+  className, 
+  maxWidth 
+}) => {
+  return (
+    <>
+      <MainContainer 
+        bgColor={ bgColor } 
+        className={ className } 
+      >
+        <Sizer maxWidth={maxWidth} />
+      </MainContainer>
+    </>
+  )
+}
+
+export default Rule
