453a3b5b9453677987bd1fc271534d2d221f0ee1
John Serrao <bigbadserrao@gmail.com>
2020-12-21 01:06:05 -0600

    Research page, as good as I can do it

---
+++ components/content/research-banners.js
@@ -0,0 +1,31 @@
+import MainContent from '../global/main-content'
+import Promo from '../slices/promo'
+
+/**
+ * <ResearchBanners>
+ * 
+ * Grid of all stats pages for the research landing page
+ *
+ * @param { array } payload - list of locations from Prismic API
+ */
+const ResearchBanners = ({
+  payload
+}) => {
+  return (
+    <MainContent>
+      { payload.map((campaign) => {
+        console.log(campaign)
+        return(
+          <Promo 
+            bigWords={ campaign.campaign.big_text }
+            path={ campaign.campaign.link }
+            smallWords={ campaign.campaign.small_text }
+            source={ campaign.campaign.banner_image.url }
+          />
+        )
+      })}
+    </MainContent>
+  )
+}
+
+export default ResearchBanners
