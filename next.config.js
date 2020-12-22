e01e8a6a618ed6b8b0fd6945d52d6acc2a4310c2
John Serrao <bigbadserrao@gmail.com>
2020-12-22 15:38:00 -0600

    Rearchitected next config file to use next-compose-plugins, now can do redirects

--- next.config.js
+++ next.config.js
@@ -1,36 +1,32 @@
 // next.config.js
+const withPlugins = require('next-compose-plugins')
 const withImages = require('next-images')
-
-const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
+const { 
+  PHASE_DEVELOPMENT_SERVER, 
+  PHASE_PRODUCTION_BUILD 
 } = require('next/constants')
+
+const nextConfig = {
+  // Set redirects here, objs in arr as needed
+  async redirects() {
+    return [
+      {
+        source: '/blog/:slug*',
+        destination: '/news/:slug*',
+        permanent: true,
+      }
+    ]
+  }
+}
 
 // This uses phases as outlined here: 
 // https://github.com/vercel/next.js/tree/canary/examples/with-env-from-next-config-js
-module.exports = withImages((phase) => {
-  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  // when `next build` or `npm run build` is used
-  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
-  // when `next build` or `npm run build` is used
-  const isStaging =
-    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+module.exports = withPlugins([
 
-  const env = {
-    SITE_URL: (() => {
-      if (isDev) return 'http://localhost:3001'
-      if (isProd) {
-        return 'https://pfb-nextjs.vercel.app/'
-      }
-      if (isStaging) return 'https://pfb-nextjs.vercel.app/'
-      return 'SITE_URL:not (isDev,isProd && !isStaging,isProd && isStaging)'
-    })(),
-  }
-
-  // next.config.js object
-  return {
-    env,
-  }
-})
-
+  // Processes images during build time
+  [withImages, {
+    [PHASE_DEVELOPMENT_SERVER]: 'http://localhost:3001',
+    [PHASE_PRODUCTION_BUILD]: 'https://www.peopleforbikes.org'
+  }]
+  
+], nextConfig)
