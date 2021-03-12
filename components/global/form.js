28fd01f4367cd55ca7220579ede4b42a3edffb62
John Serrao <bigbadserrao@gmail.com>
2021-03-12 12:00:02 -0600

    Footer sign up update

--- components/global/form.js
+++ components/global/form.js
@@ -1,8 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { useForm } from "react-hook-form";
 
-import {RedTextField} from "../primitives/text";
+import { RedTextField } from "../primitives/text";
 import InputButton from "../primitives/input-button";
 
 const FormContainer = styled.form`
@@ -14,6 +13,96 @@ const FormContainer = styled.form`
   @media (min-width: ${(props) => props.theme.lg}) {
     padding: 0;
   }
+
+
+
+  dt {
+    display: none;
+  }
+
+  label[for=cf-privacy-terms7104], 
+  label[for=cf-general-consent1667] {
+    display: inline;
+  }
+
+  input[type=text],
+  input[type=email],
+  select {
+    background-color: ${(props) => props.theme.redAccent};
+    color: white;
+
+    /* Changing placeholder text remain surreal, even in 2020 */
+    ::-webkit-input-placeholder {
+      color: white;
+    }
+    ::-moz-placeholder {
+      color: white;
+    }
+    ::-ms-input-placeholder {
+      color: white;
+    }
+    ::placeholder {
+      color: white;
+    }
+    border: none;
+    display: block;
+    font-family: ${props => props.theme.montserrat};
+    font-size: 14px;
+    font-weight: 700;
+    margin-bottom: 1vh;
+    min-width: 300px;
+    padding: 10px 16px;
+    text-transform: capitalize;
+
+    @media (min-width: ${props => props.theme.bm}) {
+      font-size: calc(14px + 4 * ((100vw - 320px) / 880));
+    }
+
+    @media (min-width: ${props => props.theme.lg}) {
+      font-size: 18px;
+    }
+  }
+
+  select {
+    width: 95%;
+  }
+
+  input[type=submit] {
+    background-color: transparent; 
+    border: 1px solid white;
+    border-radius: 0px;
+    color: white;
+    cursor: pointer;
+    display: inline-block;
+    font-family: ${props => props.theme.montserrat};
+    font-size: 14px;
+    font-weight: 700;
+    min-width: 200px;
+    margin: 2vh 0 0 0;
+    padding: 10px 35px;
+    text-align: center;
+    text-decoration: none;
+    text-transform: uppercase;
+    transform: translateY(0);
+    transition: all 0.25s ease;
+    
+    &:hover {
+      background-color: white;
+      color: ${props => props.theme.black};
+      text-decoration: none;
+      transform: translateY(-2px);
+    }
+
+    @media (min-width: ${props => props.theme.bm}) {
+      font-size: calc(14px + 4 * ((100vw - 320px) / 880));
+      padding: 10px calc(35px + 15 * ((100vw - 320px) / 880));
+    }
+
+    @media (min-width: ${props => props.theme.lg}) {
+      font-size: 18px;
+      padding: 10px 60px;
+    }
+  }
 `;
 
 const WhiteButton = styled(InputButton)`
@@ -21,35 +110,17 @@ const WhiteButton = styled(InputButton)`
 `;
 
 function SignUpForm() {
-  const { signup, handleSubmit } = useForm();
-
-  const onSignupSubmit = (data) => console.log(data); // no real processing yet
-
   return (
     <>
-      <FormContainer onSubmit={handleSubmit(onSignupSubmit)}>
-        <RedTextField
-          id="newsletter-name"
-          name="name"
-          placeholder="Name"
-          ref={signup}
-        />
-        <RedTextField
-          id="newsletter-email"
-          name="email"
-          placeholder="Email"
-          ref={signup}
-        />
-        <WhiteButton
-          buttonBg="rgba(255,255,255,0)"
-          buttonBgHover="rgba(255,255,255,1)"
-          buttonBorder="1px solid white"
-          buttonColor="white"
-          buttonColorHover="#404040"
-          buttonText="Submit"
-          name="submit"
-        />
+      <FormContainer>
+        <div id="wsd-root" class="spkactionform"></div>
+        <script
+          src="https://code.jquery.com/jquery-3.5.1.min.js"
+          integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+          crossOrigin="anonymous"></script>
+        <script src="https://action.peopleforbikes.org/assets/js/widget.js/?id=111276" async="" type="text/javascript"></script>
       </FormContainer>
+      
     </>
   );
 }
