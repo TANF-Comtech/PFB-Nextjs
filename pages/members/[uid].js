b526fa2c38b3bec9b5abf2f23bce0da95c5bcd2c
John Serrao <bigbadserrao@gmail.com>
2021-05-02 18:53:31 -0500

    Revising members so it works again

--- pages/members/[uid].js
+++ pages/members/[uid].js
@@ -165,7 +165,7 @@ export default function MembersPage({ page, preview }) {
                         );
                       })}
                     </Grid>
-                  </MainContent>
+                  </MainContent> 
                 );
               }
             })}
