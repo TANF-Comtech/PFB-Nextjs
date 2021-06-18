1bdeda781dcf68646cf04505fb40055e1697936b
John Serrao <bigbadserrao@gmail.com>
2021-06-18 12:48:11 -0500

    Got rid of useState and useEffect that was causing data issues

--- pages/members/[uid].js
+++ pages/members/[uid].js
@@ -74,19 +74,6 @@ export default function MembersPage({ page, preview }) {
   const { member_content } = page;
   const { meta } = useContext(DefaultContext);
 
-  const [theReports, setTheReports] = useState(member_content.body && member_content.body)
-
-  // Monthly sales reports array needs to be flipped around
-  // Check to see if it's here and reverse() it if it is
-  useEffect(() => {
-    // Transform state obj to just be the reports, in reverse order
-    theReports.map( slice => {
-      if ( slice.type === 'biz_intel_hub' && slice.fields.length > 1) {
-        setTheReports(slice.fields.reverse())
-      } 
-    })
-  }, [])  
-
   return (
     <>
       <script
