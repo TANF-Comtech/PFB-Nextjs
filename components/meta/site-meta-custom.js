02223f25c5beae09887455a84eab3fa0f94487d5
John Serrao <bigbadserrao@gmail.com>
2021-07-06 22:13:21 -0500

    Even more metadata

--- components/meta/site-meta-custom.js
+++ components/meta/site-meta-custom.js
@@ -31,40 +31,46 @@ const SiteMetaCustom = ( {
       <title key="title">{ title }</title>
       <meta name="description" 
             content={ desc } 
-            key="metadesc" />
+            key="metadesc" ></meta>
       <meta name="keywords" 
             content={ keywords } 
-            key="metakeywords" />            
+            key="metakeywords" ></meta>  
+      <meta property="fb:app_id" content="1771036746278242" ></meta>
+      <meta property="og:locale" content="en_US"></meta>
+      <meta property="og:site_name" content="PeopleForBikes"></meta>
+      <meta property="og:type" content="website" ></meta>
+      <meta property="og:url" content={ path } ></meta>
       <meta property="og:image" 
             content={ imgSrc }  
-            key="ogimg" />
+            key="ogimg" ></meta>
       <meta property="og:image:width" 
             content={ imgWidth } 
-            key="ogimgw" />
+            key="ogimgw" ></meta>
       <meta property="og:image:height" 
             content={ imgHeight } 
-            key="ogimgh" />
+            key="ogimgh" ></meta>
       <meta property="og:title" 
             content={ title }
-            key="ogtitle" />
+            key="ogtitle" ></meta>
       <meta property="og:description" 
             content={desc}  
-            key="ogdesc" />
+            key="ogdesc" ></meta>
       <meta property="og:url" 
             content={ path } 
-            key="ogurl" />          
+            key="ogurl" ></meta>          
       <meta name="twitter:title" 
             content={ title } 
-            key="twtrtitle" />
+            key="twtrtitle" ></meta>
       <meta name="twitter:description" 
             content={desc}
-            key="twtrdesc" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@peopleforbikes" />
-      <meta property="twitter:image" content={ imgSrc } key="twitterimg" />      
+            key="twtrdesc" ></meta>
+      <meta name="twitter:card" content="summary_large_image" ></meta>
+      <meta name="twitter:domain" content="peopleforbikes.org" ></meta>
+      <meta name="twitter:site" content="@peopleforbikes" ></meta>
+      <meta property="twitter:image" content={ imgSrc } key="twitterimg" ></meta>      
       <link rel="canonical" 
             href={ path } 
-            key="canonical" />  
+            key="canonical" ></link>  
       { ldJSON &&
         ( <script type="application/ld+json">{JSON.stringify(ldJSON)}</script> ) 
       } 
