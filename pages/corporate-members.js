4d0914a23fd197f7a987974b1aff2ed6c3fc4967
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2021-09-17 12:04:38 -0700

    :construction: Working on getting a response from salesforce.

--- pages/corporate-members.js
+++ pages/corporate-members.js
@@ -2,27 +2,33 @@ import React from "react";
 import Wrapper from "../components/global/wrapper";
 import { getAccessToken } from "../lib/salesforce/accessToken";
 import { salesforceInstanceURL } from "../lib/salesforce/checkEmailInSalesforce";
+import * as jsforce from "jsforce";
 
 export default function CorporateMembers({ data }) {
-  const { memberData } = data;
+  console.log(data);
 
-  return <Wrapper></Wrapper>;
+  return <Wrapper> {data}</Wrapper>;
 }
 
 // getStaticPaths requires a the whole paths argument to be objects of URL it needs to statically render server-side
 export async function getStaticProps() {
+  let sfCall;
+
   getAccessToken().then((data) => {
     const accessToken = data;
-    var sfConnection = new jsforce.Connection({
+    const sfConnection = new jsforce.Connection({
       instanceUrl: salesforceInstanceURL,
       version: "49.0",
       accessToken: accessToken,
+    });
+    sfConnection.query(`SELECT * FROM Account`).then(function (res) {
+      console.log(res);
     });
   });
 
   return {
     props: {
-      data: "hello",
+      data: sfCall ?? "Not Working",
     },
   };
 }
