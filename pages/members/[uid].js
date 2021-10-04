f5783bd214f437b2eeb460a4babbb21a8a7a37d4
John Serrao <bigbadserrao@gmail.com>
2021-10-04 10:06:29 -0500

    added back in commented out auth for members pages

--- pages/members/[uid].js
+++ pages/members/[uid].js
@@ -221,3 +221,52 @@ export async function getServerSideProps({
     },
   };
 } 
+
+// export async function getServerSideProps({
+//   req,
+//   res,
+//   params,
+//   preview = false,
+//   previewData,
+// }) {
+//   const cookies = new Cookies(req, res);
+//   const token = cookies.get("auth-token");
+
+//   if (token) {
+//     const data = await auth0ValidateToken(token);
+
+//     if (data.loggedIn) {
+//       const pageData = await getSingleMemberPage(params.uid, previewData);
+
+//       if(pageData.member_content.body !== null ) {
+            
+//         pageData.member_content.body.map( slice => {
+//           if ( slice.type === 'biz_intel_hub' && slice.fields.length > 1) {
+//             slice.fields.reverse()
+//           } 
+//         })
+//       }
+
+//       return {
+//         props: {
+//           preview,
+//           page: pageData ?? null,
+//         },
+//       };
+//     } else {
+//       return {
+//         redirect: {
+//           destination: "/log-in",
+//           permanent: false,
+//         },
+//       };
+//     }
+//   } else {
+//     return {
+//       redirect: {
+//         destination: "/log-in",
+//         permanent: false,
+//       },
+//     };
+//   }
+// }
