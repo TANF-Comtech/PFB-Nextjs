1bdb201dbebcbfce2ca067abfba7efd06ec93fbb
John Serrao <bigbadserrao@gmail.com>
2021-11-11 14:23:45 -0600

    safari form fixes

--- components/styles/global-css.js
+++ components/styles/global-css.js
@@ -395,6 +395,7 @@ const GlobalStyle = createGlobalStyle `
       /* Stuff just for checkboxes and switches, not radio buttons */
       &:not(.switch) {
         border-radius: 7px;
+        margin-right: 5px;
 
         &:after {
           border: 2px solid var(--active-inner);
@@ -754,6 +755,7 @@ const GlobalStyle = createGlobalStyle `
   }
 
   .spkactionform input {
+    border: 1px solid #000;
     padding: 5px 10px;
   }
 
@@ -787,6 +789,12 @@ const GlobalStyle = createGlobalStyle `
   }
 
   .spkactionform select {
+    background: url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0Ljk1IDEwIj48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6I2ZmZjt9LmNscy0ye2ZpbGw6IzQ0NDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPmFycm93czwvdGl0bGU+PHJlY3QgY2xhc3M9ImNscy0xIiB3aWR0aD0iNC45NSIgaGVpZ2h0PSIxMCIvPjxwb2x5Z29uIGNsYXNzPSJjbHMtMiIgcG9pbnRzPSIxLjQxIDQuNjcgMi40OCAzLjE4IDMuNTQgNC42NyAxLjQxIDQuNjciLz48cG9seWdvbiBjbGFzcz0iY2xzLTIiIHBvaW50cz0iMy41NCA1LjMzIDIuNDggNi44MiAxLjQxIDUuMzMgMy41NCA1LjMzIi8+PC9zdmc+) no-repeat 95% 50%;
+    -moz-appearance: none; 
+    -webkit-appearance: none; 
+    appearance: none;
+    border: 1px solid #000;
+    border-radius: 0px;
     padding: 5px 10px;
   }
 
