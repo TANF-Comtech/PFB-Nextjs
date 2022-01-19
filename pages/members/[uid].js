f5c296ebfe94c7d00d7284795cfe19dbbb460f3e
John Serrao <bigbadserrao@gmail.com>
2022-01-19 17:20:07 -0600

    data studio SRC now goes to first page

--- pages/members/[uid].js
+++ pages/members/[uid].js
@@ -146,7 +146,7 @@ export default function MembersPage({ page, preview }) {
               <iframe 
                 width="100%" 
                 height="8000" 
-                src="https://datastudio.google.com/embed/reporting/fe9c9fc6-ee8b-43a6-ae3a-1a2f20377a04/page/WGFSB" 
+                src="https://datastudio.google.com/embed/reporting/fe9c9fc6-ee8b-43a6-ae3a-1a2f20377a04" 
                 frameborder="0" 
                 allowFullScreen="allowfullscreen"
                 aria-label="PeopleForBikes Business Intelligence Hub">
