77d10182f3cc0d2af810571c17ea966d1fd61b84
John Serrao <bigbadserrao@gmail.com>
2022-03-03 15:48:04 -0600

    DoubleTheDonation page

---
+++ components/content/double-the-donation.js
@@ -0,0 +1,78 @@
+import styled from "styled-components"
+
+import MainContent from "../global/main-content"
+
+const Container = styled.section`
+  .embed-container {
+    position: relative; 
+    padding-bottom: 80%; 
+    height: 0; 
+    max-width: 100%;
+  } 
+  
+  .embed-container iframe, 
+  .embed-container object, 
+  .embed-container iframe{
+    position: absolute; 
+    top: 0; 
+    left: 0; 
+    width: 100%; 
+    height: 100%;
+  } 
+  
+  small{
+    position: absolute; 
+    z-index: 40; 
+    bottom: 0; 
+    margin-bottom: -15px;
+  }
+`
+
+const RedHeading = styled.h2`
+  color: ${ props => props.theme.red };
+  font-weight: 700;
+  text-transform: uppercase;
+`
+
+/**
+ * <DoubleTheDonation>
+ * 
+ * Static iFrame of the DoubleTheDonation service
+ * This was only designed to use on /pages/double-your-donation.js
+ */
+export default function DoubleTheDonation() {
+
+  // API Key for the DoubleTheDonation Script
+  let DDCONF = { API_KEY: "R1fTLU2ixqyOvcdB" }
+
+  return (
+    <>
+      <MainContent
+        contentPadding="2vh 4vw 2vh 4vw"
+        maxWidth="1200px"
+      >
+        <RedHeading>Double Your Donation to PeopleForBikes</RedHeading>
+        <hr />
+        <p>Did you know that <strong>thousands of companies match donations by employees</strong> to PeopleForBikes? Please use the search tool below to see if your company will match your donation and the <strong>follow the provided links if they do</strong>.</p>
+      </MainContent>
+      <MainContent
+        contentPadding="0vh 4vw 4vh 4vw"
+        maxWidth="800px"
+      >
+      <Container>
+        <script src="https://doublethedonation.com/api/js/ddplugin.js"></script> 
+        <div id="dd-container">
+          <a href="https://doublethedonation.com/matching-grant-resources/matching-gift-basics/">Matching Gift</a> and <a href="https://doublethedonation.com/matching-grant-resources/volunteer-grant-basics/">Volunteer Grant</a> information provided by <br />
+          <a href="https://doublethedonation.com"><img alt="Powered by Double the Donation" src="https://doublethedonation.com/api/img/powered-by.png" /></a>
+        </div>
+      </Container>
+      </MainContent>
+      <MainContent
+        contentPadding="4vh 4vw 2vh 4vw"
+        maxWidth="1200px"
+      >
+        <p style={{ textAlign: "center" }}>Questions about this page? Reach out to <a href="mailto:info@peopleforbikes.org?subject=Questions%20about%20PeopleForBikes%20DoubleTheDonation%20Service">our team</a> for more details.</p>
+      </MainContent>
+    </>
+  )
+}
